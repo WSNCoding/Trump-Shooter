1e786ed9b2e1293cbf544f85a44d6d75ab0b6e38
NinjutsuFish <formaggio@hotmail.co.uk>
2016-03-03 17:49:08 +0000

    Update TKINTER.BUTTONS.AND.LBLS.py

--- Trump Shooter/Trump-Shooter/Scripts/TKINTER.BUTTONS.AND.LBLS.py
+++ Trump Shooter/Trump-Shooter/Scripts/TKINTER.BUTTONS.AND.LBLS.py
@@ -2,7 +2,7 @@ import tkinter as tkinter
 from tkinter import *
 
 root = tkinter.Tk()
-root.title("Ollie")# running a window
+root.title("Ollie is the true gay lord and wants to suck deez nutz over and over again")# running a window
 root.geometry("300x300")
 canvas = tkinter.Canvas(root, width=100, height=100)
 
