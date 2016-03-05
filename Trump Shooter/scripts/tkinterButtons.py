580b6517bf1ad1295070c4c1ce62075fd8826799
TheMiner63 <cboddy1@hotmail.com>
2016-03-05 08:41:18 +0000

    Changes from client
    I have changed windowCode() to a class so that it may call tkinter
    without trouble. This means that main.py needs to call it now with an
    instance. It should run when called without need for calling a method.

--- Trump Shooter/scripts/tkinterButtons.py
+++ Trump Shooter/scripts/tkinterButtons.py
@@ -1,20 +1,22 @@
-import tkinter as tkinter
-from tkinter import *
+import tkinter
 
-def windowCode():
-    root = tkinter.Tk()
-    root.title("Game")# running a window
-    root.geometry("300x300")
-    canvas = tkinter.Canvas(root, width=100, height=100)
+class windowCode():
+    def _init_(self):
+        root = tkinter.Tk()
+        root.title("Game")# running a window
+        root.geometry("300x300")
+        canvas = tkinter.Canvas(root, width=100, height=100)
 
-    frame = Frame(root)
+        frame = Frame(root)
 
-    frame.grid()
-    lbl = Label(frame, text = "Press the button below to DO STUFF")#Labels
-    lbl.grid()
+        frame.grid()
+        lbl = Label(frame, text = "Press the button below to DO STUFF")#Labels
+        lbl.grid()
 
-    bttn = Button(frame, text="Click me")#Buttons
-    bttn.grid()
+        bttn = Button(frame, text="Click me")#Buttons
+        bttn.grid()
 
-    im = Image.open("\textures\GetAttachment.jpg");
-    myImage.show();
+        im = Image.open("\textures\GetAttachment.jpg")
+        myImage.show()
+
+run = windowCode()
