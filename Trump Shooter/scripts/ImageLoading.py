98fa1fd9d84f27c7753afbb36c04e13ac2a8d911
TheMiner63 <cboddy1@hotmail.com>
2016-03-11 18:48:33 +0000

    Minor

---
+++ Trump Shooter/scripts/ImageLoading.py
@@ -0,0 +1,26 @@
+import tkinter
+def hideBG():
+    global state
+    if state == "Hidden":
+        background_label.pack()
+        state = "Showing"
+
+    elif state == "Showing":
+        background_label.pack_forget()
+        state = "Hidden"
+
+
+
+
+window = tkinter.Tk()
+
+background_image=tkinter.PhotoImage(file="Example.png")
+background_label = tkinter.Label(window, image=background_image)
+
+hideBttn = tkinter.Button(window, text="Hide Background", command=hideBG)
+state = "Showing"
+
+hideBttn.pack()
+background_label.pack()
+
+window.mainloop()
